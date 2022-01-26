5ce930414e0cd644e057fdedaa247fc69cc5077c
mizzKarenbabii <mizzkarenbabii@gmail.com>
2022-01-25 22:12:37 -0500

    finished lesson 1

---
+++ app.js
@@ -0,0 +1,19 @@
+
+
+
+// const printProfileData = profileDataArr => {
+//     // This...
+//     for (let i = 0; i < profileDataArr.length; i += 1) {
+//       console.log(profileDataArr[i]);
+//     }
+  
+//     console.log('================');
+  
+    // Is the same as this...
+    profileDataArr.forEach((profileItem) => {
+      console.log(profileItem)
+    });
+
+
+const generatePage = () => 'Name :James, Github:janehub';
+console.log(generatePage());
